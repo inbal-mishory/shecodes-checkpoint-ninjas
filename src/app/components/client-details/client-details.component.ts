819b6c3d844c054e2b116b8725151ba153ad1cc0
inbal-mishory <mishory@gmail.com>
2018-02-11 22:27:55 +0200

    Changed project title

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../services/client.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { FlashMessagesService } from 'angular2-flash-messages'
-import { Client } from '../../models/Client';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from '../../models/Client'; 
 
 @Component({
   selector: 'app-client-details',
@@ -27,7 +27,17 @@ export class ClientDetailsComponent implements OnInit {
     // Get client
     this.clientService.getClient(this.id).subscribe(client => {
       this.client = client;
-    })
+    });
+  }
+
+  onDeleteClick(){
+    if(confirm('Are you sure you want to delete this member?')){
+      this.clientService.deleteClient(this.client);
+      this.flashMessage.show('Member removed', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/']);
+    }
   }
 
 }
